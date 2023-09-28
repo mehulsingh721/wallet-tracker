65dd06da5ecd5c37541222ccd4865ac39de02d33
mehulsingh721 <mehulsingh072001@gmail.com>
2023-09-28 19:36:36 +0530

    first commit

--- src/listener.ts
+++ src/listener.ts
@@ -9,140 +9,128 @@ import { saveNft } from "./helpers/nft.helper";
 const zeroth = "0x0000000000000000000000000000000000000000";
 
 export const erc20TransferEventListener = async () => {
-  const currentBlockNumber = await publicClient.getBlockNumber();
+  try {
+    const currentBlockNumber = await publicClient.getBlockNumber();
 
-  for (let i = BigInt(18219792); i < BigInt(currentBlockNumber); i++) {
-    const logs = await publicClient.getLogs({
-      event: parseAbiItem(
-        "event Transfer(address indexed, address indexed, uint)"
-      ),
-      fromBlock: BigInt(i),
-      toBlock: BigInt(i),
-    });
+    for (let i = BigInt(0); i < BigInt(currentBlockNumber); i++) {
+      const logs = await publicClient.getLogs({
+        event: parseAbiItem(
+          "event Transfer(address indexed, address indexed, uint)"
+        ),
+        fromBlock: BigInt(i),
+        toBlock: BigInt(i),
+      });
 
-    if (logs.length !== 0) {
-      for (let j = 0; j < logs.length; j++) {
-        if (logs[j].args.length > 1) {
-          const token: string = logs[j]["address"];
-          const from = logs[j].args[0];
-          const to = logs[j].args[1];
-          await saveErc20Token(from, token, -1);
-          await saveErc20Token(to, token, 1);
+      if (logs.length !== 0) {
+        for (let j = 0; j < logs.length; j++) {
+          if (logs[j].args.length > 1) {
+            const token: string = logs[j]["address"];
+            const from = logs[j].args[0];
+            const to = logs[j].args[1];
+            await saveErc20Token(from, token, -1);
+            await saveErc20Token(to, token, 1);
+          }
         }
       }
     }
+    console.log("Indexing Done");
+  } catch (err) {
+    console.log(err);
   }
-  console.log("Indexing Done");
 };
 export const ethTransferEventListener = async () => {
-  const currentBlockNumber = await publicClient.getBlockNumber();
+  try {
+    const currentBlockNumber = await publicClient.getBlockNumber();
 
-  for (let i = BigInt(18219792); i < BigInt(currentBlockNumber); i++) {
-    const block = await publicClient.getBlock({
-      blockNumber: i,
-    });
-    const transactions = block.transactions;
+    for (let i = BigInt(0); i < BigInt(currentBlockNumber); i++) {
+      const block = await publicClient.getBlock({
+        blockNumber: i,
+      });
+      const transactions = block.transactions;
 
-    // console.log(i)
-    if (transactions.length !== 0) {
-      for (let j = 0; j < transactions.length; j++) {
-        const transaction = await publicClient.getTransaction({
-          hash: transactions[j],
-        });
-        const token: string = "native";
-        const from = transaction?.from?.toLowerCase();
-        const to = transaction?.to?.toLowerCase();
-        const amount = transaction.value;
-        await saveEthToken(from, token, BigInt(1), "sub");
-        await saveEthToken(to, token, BigInt(1), "add");
+      // console.log(i)
+      if (transactions.length !== 0) {
+        for (let j = 0; j < transactions.length; j++) {
+          const transaction = await publicClient.getTransaction({
+            hash: transactions[j],
+          });
+          const token: string = "native";
+          const from = transaction?.from?.toLowerCase();
+          const to = transaction?.to?.toLowerCase();
+          const amount = transaction.value;
+          await saveEthToken(from, token, BigInt(1), "sub");
+          await saveEthToken(to, token, BigInt(1), "add");
+        }
       }
     }
+  } catch (err) {
+    console.log(err);
   }
 };
 
 export const nft721TrasferEventListener = async () => {
-  const event =
-    "event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId)";
-  const currentBlockNumber = await publicClient.getBlockNumber();
+  try {
+    const event =
+      "event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId)";
+    const currentBlockNumber = await publicClient.getBlockNumber();
 
-  for (let i = BigInt(18219792); i < BigInt(currentBlockNumber); i++) {
-    const logs = await publicClient.getLogs({
-      event: parseAbiItem(event),
-      fromBlock: BigInt(i),
-      toBlock: BigInt(i),
-    });
+    for (let i = BigInt(0); i < BigInt(currentBlockNumber); i++) {
+      const logs = await publicClient.getLogs({
+        event: parseAbiItem(event),
+        fromBlock: BigInt(i),
+        toBlock: BigInt(i),
+      });
 
-    if (logs.length !== 0) {
-      for (let j = 0; j < logs.length; j++) {
-        const collection: string = logs[j]["address"];
-        const from = logs[j].args["_from"];
-        const to = logs[j].args["_to"];
-        const toknenId = logs[j].args["_tokenId"];
+      if (logs.length !== 0) {
+        for (let j = 0; j < logs.length; j++) {
+          const collection: string = logs[j]["address"];
+          const from = logs[j].args["_from"];
+          const to = logs[j].args["_to"];
+          const toknenId = logs[j].args["_tokenId"];
 
-        if (toknenId && to && from) {
-          if (from !== zeroth) {
-            await saveNft(collection, from, toknenId.toString(), -1, "ERC721");
+          if (toknenId && to && from) {
+            if (from !== zeroth) {
+              await saveNft(
+                collection,
+                from,
+                toknenId.toString(),
+                -1,
+                "ERC721"
+              );
+            }
+            await saveNft(collection, to, toknenId.toString(), 1, "ERC721");
           }
-          await saveNft(collection, to, toknenId.toString(), 1, "ERC721");
         }
       }
     }
+  } catch (err) {
+    console.log(err);
   }
 };
 
 export const nft1155TrasferEventListener = async () => {
-  const event1 =
-    "event TransferSingle(address indexed operator, address indexed from, address indexed to, uint256 id, uint256 value)";
-  const event2 =
-    "TransferBatch(address indexed operator, address indexed from, address indexed to, uint256[] ids, uint256[] values)";
-  const currentBlockNumber = await publicClient.getBlockNumber();
+  try {
+    const event1 =
+      "event TransferSingle(address indexed operator, address indexed from, address indexed to, uint256 id, uint256 value)";
+    const event2 =
+      "TransferBatch(address indexed operator, address indexed from, address indexed to, uint256[] ids, uint256[] values)";
+    const currentBlockNumber = await publicClient.getBlockNumber();
 
-  for (let i = BigInt(18221759); i < BigInt(currentBlockNumber); i++) {
-    const logs = await publicClient.getLogs({
-      event: parseAbiItem(event1),
-      fromBlock: BigInt(i),
-      toBlock: BigInt(i),
-    });
+    for (let i = BigInt(0); i < BigInt(currentBlockNumber); i++) {
+      const logs = await publicClient.getLogs({
+        event: parseAbiItem(event1),
+        fromBlock: BigInt(i),
+        toBlock: BigInt(i),
+      });
 
-    if (logs.length !== 0) {
-      for (let j = 0; j < logs.length; j++) {
-        console.log(logs[j]);
-        const collection: string = logs[j].args.operator;
-        const from = logs[j].args["from"];
-        const to = logs[j].args["to"];
-        const toknenId = logs[j].args["id"].toString();
-        const quantity = logs[j].args["value"];
-        if (from !== zeroth) {
-          await saveNft(collection, from, toknenId, -quantity, "ERC1155");
-        }
-        await saveNft(collection, to, toknenId, quantity, "ERC1155");
-      }
-    }
-  }
-};
-
-export const nft1155BatchTrasferEventListener = async () => {
-  const event2 =
-    "event TransferBatch(address indexed operator, address indexed from, address indexed to, uint256[] ids, uint256[] values)";
-  const currentBlockNumber = await publicClient.getBlockNumber();
-
-  for (let i = BigInt(18219792); i < BigInt(currentBlockNumber); i++) {
-    const logs = await publicClient.getLogs({
-      event: parseAbiItem(event2),
-      fromBlock: BigInt(i),
-      toBlock: BigInt(i),
-    });
-
-    if (logs.length !== 0) {
-      for (let j = 0; j < logs.length; j++) {
-        const collection: string = logs[j].args.operator;
-        const from = logs[j].args["from"];
-        const to = logs[j].args["to"];
-        const ids = logs[j].args.ids;
-        for (let i = 0; i < ids.length; i++) {
-          const toknenId = ids[i].toString();
-          const quantity = logs[i];
-
+      if (logs.length !== 0) {
+        for (let j = 0; j < logs.length; j++) {
+          console.log(logs[j]);
+          const collection: string = logs[j].args.operator;
+          const from = logs[j].args["from"];
+          const to = logs[j].args["to"];
+          const toknenId = logs[j].args["id"].toString();
+          const quantity = logs[j].args["value"];
           if (from !== zeroth) {
             await saveNft(collection, from, toknenId, -quantity, "ERC1155");
           }
@@ -150,5 +138,44 @@ export const nft1155BatchTrasferEventListener = async () => {
         }
       }
     }
+  } catch (err) {
+    console.log(err);
   }
 };
+
+export const nft1155BatchTrasferEventListener = async () => {
+  try {
+    const event2 =
+      "event TransferBatch(address indexed operator, address indexed from, address indexed to, uint256[] ids, uint256[] values)";
+    const currentBlockNumber = await publicClient.getBlockNumber();
+
+    for (let i = BigInt(0); i < BigInt(currentBlockNumber); i++) {
+      const logs = await publicClient.getLogs({
+        event: parseAbiItem(event2),
+        fromBlock: BigInt(i),
+        toBlock: BigInt(i),
+      });
+
+      if (logs.length !== 0) {
+        for (let j = 0; j < logs.length; j++) {
+          const collection: string = logs[j].args.operator;
+          const from = logs[j].args["from"];
+          const to = logs[j].args["to"];
+          const ids = logs[j].args.ids;
+          for (let i = 0; i < ids.length; i++) {
+            const toknenId = ids[i].toString();
+            const quantity = logs[i];
+
+            if (from !== zeroth) {
+              await saveNft(collection, from, toknenId, -quantity, "ERC1155");
+            }
+            await saveNft(collection, to, toknenId, quantity, "ERC1155");
+          }
+        }
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+// 18219792
